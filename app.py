9ab076831a53cc2d7ec8ec7f9d6b981aadaf79b2
Kaushal kulkarni <kaushalvk0@gmail.com>
2026-08-20 20:00:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import google.genrativeai as gneai
 from fpdf import FPDF
 from PIL import image
+st.set_page_config(page_title="Automated pdf Billing", layout="centered")
+st.title("Purchase order to PDF bill")
+st.write("Upload Purchase Order")
+uploded_file= st.file_uploader("upload po here (PDF or IMG)" , type=["jpg", "jpeg", "png", "pdf"])
+if uploaded_file  is not none:
+  st.success("File succefully uploaded! processing......")
+  
 
