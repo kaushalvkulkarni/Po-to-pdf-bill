268573b506994abce5fa0996e4dd193753c587b8
Kaushal kulkarni <kaushalvk0@gmail.com>
2026-08-20 20:19:42 +0530

    Fix typo in variable name for uploaded file

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.set_page_config(page_title="Automated pdf Billing", layout="centered")
 st.title("Purchase order to PDF bill")
 st.write("Upload Purchase Order")
-uploded_file= st.file_uploader("upload po here (PDF or IMG)" , type=["jpg", "jpeg", "png", "pdf"])
+uploaded_file= st.file_uploader("upload po here (PDF or IMG)" , type=["jpg", "jpeg", "png", "pdf"])
 if uploaded_file  is not none:
   st.success("File succefully uploaded! processing......")
   
