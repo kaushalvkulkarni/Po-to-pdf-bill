67d3c25ac804ca51a74e2837011a8a13766fb565
Kaushal kulkarni <kaushalvk0@gmail.com>
2026-08-21 20:27:03 +0530

    Remove unused code for file uploader in app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-import streamlit as st 
-import google.generativeai as genai
-from fpdf import FPDF
-from PIL import Image
-st.set_page_config(page_title="Automated pdf Billing", layout="centered")
-st.title("Purchase order to PDF bill")
-st.write("Upload Purchase Order")
-uploaded_file= st.file_uploader("Upload PO here (PDF or IMG)" , type=["jpg", "jpeg", "png", "pdf"])
-if uploaded_file  is not None:
-  st.success("File succefully uploaded! processing......") 
+
 
 
 import streamlit as st
