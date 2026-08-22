ab7de782869f7094d2066a839d8c835fc06776d5
Kaushal kulkarni <kaushalvk0@gmail.com>
2026-08-22 17:58:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from fpdf import FPDF
 import json
 import os
 import tempfile
+client=genai.client(api_key=st.secreat["GEMINI_API_KEY"])
 
 # Set page configuration
 st.set_page_config(page_title="PO to Tax Invoice Generator", layout="centered")
