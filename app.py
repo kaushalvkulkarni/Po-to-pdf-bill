2b78db48c304c933accb4a859c98b6552f6b01b9
Kaushal kulkarni <kaushalvk0@gmail.com>
2026-08-22 18:15:07 +0530

    Fix API key secret reference in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-api_key=st.secrets["GEMINI-API_KEY"]
+api_key=st.secrets["GEMINI_API_KEY"]
 genai.configure(api_key=api_key)
 from fpdf import FPDF
 import json
